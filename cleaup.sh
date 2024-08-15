da5cb561780960b88ef85e54758cba6718dfcdb5
Vishal Mishra <vishal.mishra84@gmail.com>
2024-08-15 13:16:14 +0400

    Updated the cleanup file

--- cleaup.sh
+++ cleaup.sh
@@ -1,1 +1,8 @@
-find /Users/vishalmishra/workspace/self/azure-cognative-example -type f ! -name 'image_analysis.ipynb' ! -path '*/venv/*' ! -name 'sample_image.png' ! -name cleaup.sh ! -name requirements.txt -delete 
+find /Users/vishalmishra/workspace/self/azure-cognative-example -type f \
+ ! -name '.gitignore' \
+ ! -name 'image_analysis.ipynb' \
+ ! -path '*/venv/*' \
+ ! -path '*/.git/*' \
+ ! -name 'sample_image.png' \
+ ! -name cleaup.sh \
+ ! -name requirements.txt -delete 
